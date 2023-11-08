48d4f045be94fb70be9b1775c0e657d5709802e4
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-08 11:41:50 -0600

    Finished ex 1.1.11

---
+++ src/main/java/chapter1/section1/ex11.java
@@ -0,0 +1,54 @@
+package chapter1.section1;
+
+public class ex11 {
+    /*1.1.11 Write a code fragment that prints the contents of a two-dimensional boolean array,
+    * using '*' to represent true and a space to represent false. Include row and column numbers. */
+    public static void main(String[] args){
+        Boolean[][] myBooleanArray = new Boolean[4][4];
+        myBooleanArray[0][0] = true;
+        myBooleanArray[0][1] = false;
+        myBooleanArray[0][2] = true;
+        myBooleanArray[0][3] = false;
+
+        myBooleanArray[1][0] = false;
+        myBooleanArray[1][1] = true;
+        myBooleanArray[1][2] = false;
+        myBooleanArray[1][3] = true;
+
+        myBooleanArray[2][0] = true;
+        myBooleanArray[2][1] = false;
+        myBooleanArray[2][2] = true;
+        myBooleanArray[2][3] = false;
+
+        myBooleanArray[3][0] = true;
+        myBooleanArray[3][1] = true;
+        myBooleanArray[3][2] = true;
+        myBooleanArray[3][3] = true;
+
+        twoDArray(myBooleanArray);
+    }
+
+    public static void twoDArray(Boolean[][] arr){
+        for(int k = 1; k < arr[0].length+1; k++){
+            if (k == 1){
+                System.out.print("   ");
+            }
+            System.out.print("C-" + k + " ");
+        }
+
+        for (int i = 0; i < arr.length; i++){
+            System.out.print("\nR-" + (i+1));
+
+            for (int j = 0; j < arr[0].length; j++){
+                if (arr[i][j]){
+                    System.out.print(" *");
+                }
+                else{
+                    System.out.print("  ");
+                }
+                System.out.print("  ");
+            }
+
+        }
+    }
+}
