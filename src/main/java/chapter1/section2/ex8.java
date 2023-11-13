67f24c0dbfc42a6d6542d5cd221e0e492394665f
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-13 09:39:52 -0600

    Finished ex 1.2.8

---
+++ src/main/java/chapter1/section2/ex8.java
@@ -0,0 +1,17 @@
+package chapter1.section2;
+
+public class ex8 {
+    /* 1.2.8 Suppose that a[] and b[] are each integer arrays consisting of millions of integers.
+    * What does the following code do? Is it reasonably efficient? */
+    public static void main(String[] args){
+        int[] a;
+        int[] b;
+
+        int t = a;
+        a = b;
+        b = t;
+
+        // The code swaps the array references of variables a and b. It is reasonably efficient as the code copies the references rather than recreating them.
+
+    }
+}
