bb236b52cef37b39807e1f390ab04f4d06078a09
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-25 15:17:17 -0600

    finished ex6

---
+++ src/main/java/chapter1/section3/ex6.java
@@ -0,0 +1,23 @@
+package chapter1.section3;
+
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+public class ex6 {
+    /* 1.3.6 What does the following code fragment do to the queue q? */
+    public static void main(String[] args){
+        /*Stack<String> stack = new Stack<String>();
+
+        while(!q.isEmpty()){
+            stack.push(q.dequeue());
+        }
+
+        while(!stack.isEmpty()){
+            q.enqueue(stack.pop());
+        }
+        */
+    }
+
+    //The code inverts the order of the data in the queue.
+}
