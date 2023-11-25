91ffaf80b82bf862d790db25eb3e3cebc915520c
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-25 16:27:32 -0600

    finished ex18

---
+++ src/main/java/chapter1/section3/ex18.java
@@ -0,0 +1,11 @@
+package chapter1.section3;
+
+public class ex18 {
+    /* 1.3.18 Suppose x is a linked-list node and not the last node on the list.
+    * What is the effect of the following code fragment? */
+    public static void main(String[] args){
+        //x.next = x.next.next;
+
+        //The code fragment deletes the node following x.
+    }
+}
