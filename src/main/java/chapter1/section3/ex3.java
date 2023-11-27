eaca7010923b9ee1a6eaaf2c30fd6f100b69cde1
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-27 15:56:41 -0600

    finished ex3

---
+++ src/main/java/chapter1/section3/ex3.java
@@ -0,0 +1,18 @@
+package chapter1.section3;
+
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+public class ex3 {
+    /* Suppose that a client performs an intermixed sequence of (stack) push and
+    * pop operations. The push operations put the integers 0 through 9 in order onto the stack;
+    * the pop operations print out the return values. Which of the following sequence(s) could not occur? */
+    public static void main(String[] args){
+        //b. 4 6 8 7 5 3 2 9 0 1
+        //f. 0 4 6 5 3 8 1 7 2 9
+        //g. 1 4 7 9 8 6 5 3 0 2
+
+        //b f and g can not occur.
+    }
+
+}
