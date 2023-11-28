cddea3faf2db29105755b27285c32be8eaf31c5d
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-27 22:39:34 -0600

    finished ex23

---
+++ src/main/java/chapter1/section3/ex23.java
@@ -0,0 +1,16 @@
+package chapter1.section3;
+
+
+public class ex23 {
+    /* 1.3.23 Why does the following code fragment not do the same thing as in the previous question? */
+
+    //x.next = t;
+    //t.next = x.next;
+
+    //The order in which the statements occur affects what node comes after which.
+
+
+    public static void main(String[] args){
+
+    }
+}
