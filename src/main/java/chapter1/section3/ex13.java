80cc889a7dc095ca2e8f4b59863ecb4c6f155ea9
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-27 21:47:33 -0600

    finished ex13

---
+++ src/main/java/chapter1/section3/ex13.java
@@ -0,0 +1,15 @@
+package chapter1.section3;
+
+public class ex13 {
+    /* 1.3.13 Suppose that a client performs an intermixed sequence of (queue) enqueue and
+    * dequeue operations. The enqueue operations put the integers 0 through 9 in order onto the queue;
+    * the dequeue operations print out the return values. Which of the following sequence(s) could not occur? */
+    public static void main(String[] args){
+        //b. 4 6 8 7 5 3 2 9 0 1
+        //c. 2 5 6 7 4 8 9 3 1 0
+        //d. 4 3 2 1 0 5 6 7 8 9
+
+        //b c and d can not occur.
+    }
+
+}
