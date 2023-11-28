804b37a1e061f655e4b9f341a96ccaeee340ef52
Henry Nguyen <nguyenhenry682@gmail.com>
2023-11-27 22:36:19 -0600

    finished ex22

---
+++ src/main/java/chapter1/section3/ex22.java
@@ -0,0 +1,18 @@
+package chapter1.section3;
+
+
+import java.util.LinkedList;
+
+public class ex22 {
+    /* 1.3.22 Suppose that x is a linked list Node. What does the following code fragment do? */
+
+    //t.next = x.next;
+    //x.next = t;
+
+    //The code fragment places t after node x.
+
+
+    public static void main(String[] args){
+
+    }
+}
